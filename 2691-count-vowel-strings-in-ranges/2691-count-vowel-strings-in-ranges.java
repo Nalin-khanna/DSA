8cb5f0b7d19503efad2c2beb9c524e394010823b
Nalin Khanna <129287466+Nalin-khanna@users.noreply.github.com>
2025-01-02 16:40:17 +0530

    Time: 8 ms (48.29%), Space: 89 MB (14.58%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,26 +1,25 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
-        String vowels = "aeiou";
+        int[] Prefixsum = new int[words.length];
+        int sum = 0 ; 
         int[] ans = new int[queries.length];
-        int[] temp = new int [words.length];
-        for (int k =0 ; k<words.length; k++){
-            char first = words[k].charAt(0);
-            char last = words[k].charAt(words[k].length() -1);
-            if(vowels.indexOf(first)!=-1 && vowels.indexOf(last)!=-1){
-                temp[k] = 1;
+        String vowel = "aeiou";
+        for(int i = 0 ; i<words.length; i++){
+            char first = words[i].charAt(0);
+            char last = words[i].charAt(words[i].length()-1);
+            if(vowel.indexOf(first)!=-1 && vowel.indexOf(last)!=-1){
+                sum = sum +1 ; 
+                 
             }
-            else{
-                temp[k] = 0 ; 
-            }
+            Prefixsum[i] = sum ;
         }
-        for(int i = 0 ; i<queries.length;i++){
-            int count = 0 ; 
-            
-            for(int j = queries[i][0]; j<=queries[i][1];j++){
-                count = count + temp[j];
-            }
-            ans[i] = count ; 
+        for(int j = 0 ; j<queries.length; j++){
+
+            int[] currentQuery = queries[j];
+            ans[j] =
+                Prefixsum[currentQuery[1]] -
+                (currentQuery[0] == 0 ? 0 : Prefixsum[currentQuery[0] - 1]);
         }
-        return ans ; 
+        return ans ;
     }
 }
