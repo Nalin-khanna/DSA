aa239797e3dcaa422665b328db5558304aabbd45
Nalin Khanna <129287466+Nalin-khanna@users.noreply.github.com>
2025-02-12 06:31:34 +0530

    Time: 0 ms (100%), Space: 44.5 MB (61.59%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashMap<ListNode , Integer> map = new HashMap<>();
-        ListNode temp = head ; 
-        while(temp!=null){
-            if(map.containsKey(temp)){
-                return true ; 
-            }else{
-                map.put(temp,1);
+        ListNode fast = head ; 
+        ListNode slow = head ;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                return true;
             }
-            temp = temp.next;
         }
         return false;
     }
